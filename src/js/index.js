541a74f8b29b6d0d90be095718d8fed297376d16
Haiyang Hong <hong961127@gmail.com>
2020-11-23 01:45:45 -0800

    Finsih mobile menu funcitonality

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,13 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import "../sass/main.scss";
+
+const menuOpenBtn = document.querySelector(".nav__toggle--menu");
+const menuCloseBtn = document.querySelector(".nav__toggle--close");
+const nav = document.querySelector(".nav");
+
+[menuOpenBtn, menuCloseBtn].forEach((btn) =>
+  btn.addEventListener("click", function () {
+    nav.classList.toggle("nav--active");
+  })
+);
